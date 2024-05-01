9a02305041dbe5595d65ba210c62412de8a32456
Andrii Suprun <Andrii.Suprun@niceincontact.com>
2024-05-01 17:10:53 -0600

    added test plugin from docs

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 import fs from 'fs';
 import * as parser from '@babel/parser';
-import * as babelTraverse from '@babel/traverse';
+import traverse from '@babel/traverse';
+import * as t from "@babel/types";
+import generate from "@babel/generator";
 
-// import traverse from '@babel/traverse';
-import { default as generate } from '@babel/generator';
-
-const traverse = babelTraverse.default.default;
-
+console.log(generate.default);
 function replaceQuotes(code) {
     const ast = parser.parse(code, {
         sourceType: 'module',
-        plugins: ['jsx', 'typescript'], // Add plugins if needed
+        plugins: ['typescript'], // Add plugins if needed
     });
 
     // Traverse the AST to replace double quotes with single quotes
-    traverse(ast, {
-        StringLiteral(path) {
-            path.node.value = path.node.value.replace(/"/g, "'");
-        },
-        // Remove space after //
-        CommentLine(path) {
-            if (path.node.type === 'Line' && path.node.value.startsWith('// ')) {
-                path.node.value = '//' + path.node.value.slice(3);
+    traverse.default(ast, {
+        BinaryExpression(path) {
+            if (path.node.operator !== "===") {
+                return;
             }
-        },
+            path.node.left = t.identifier("sebmck");
+            path.node.right = t.identifier("dork");
+        }
     });
 
     // Generate code from the modified AST
-    const { code: formattedCode } = generator(ast);
+    const { code: formattedCode } = generate.default(ast);
 
     return formattedCode;
 }
@@ -48,6 +44,7 @@ function formatCode(filePath) {
         console.error('Error:', error.message);
     }
 }
+
 // Example usage
 const filePath = 'examples/1.ts';
 formatCode(filePath);
